01cf4283754a86e9c445e56cfd5c1701f8d383c2
EthanHindmarsh <ethan@ethan.lol>
2022-04-02 12:25:22 -0500

    Turn project into cargo workspace

--- abcproblem/rust/tests/lib_tests.rs
+++ abcproblem/rust/tests/lib_tests.rs
@@ -1,6 +1,16 @@
 #![cfg_attr(feature = "strict", deny(warnings))]
 
+// ---- Testing Outline ----
+//
+// 1. Major Tests: (Test the behaviour of the library, or test any requirements outlined in README.md)
+//    - Test that the library is able to actually solve the abc problem
+//    - Test against the problems in README.md
+// 2. Minor Tests: (Per-function tests, implementation-specific. Not BDD, so can be replaced if implementation changes.)
+//    - Example: Test that sort_characters_by_frequency() returns a correctly sorted vec.
+//
+// ---- End of Test Outline ----
+
 #[test]
-fn life_the_universe_and_everything() {
-    assert_eq!(1, abc::answer());
+fn test_lib_test() {
+    assert_eq!(1, 1);
 }
