362a38f1f67596d15415f299f28f7788e469661f
EthanHindmarsh <ethan@ethan.lol>
2022-04-03 16:07:42 -0500

    Finish ABCProblem in Rust, significantly shorter than Python's solution

--- abcproblem/rust/src/lib.rs
+++ abcproblem/rust/src/lib.rs
@@ -1,5 +1,41 @@
 #![cfg_attr(feature = "strict", deny(warnings))]
 
-pub fn answer() -> i32 {
-    1
+const LETTER_PAIRS: [(&str, &str); 20] = [
+    ("B", "O"),
+    ("X", "K"),
+    ("D", "Q"),
+    ("C", "P"),
+    ("N", "A"),
+    ("G", "T"),
+    ("R", "E"),
+    ("T", "G"),
+    ("Q", "D"),
+    ("F", "S"),
+    ("J", "W"),
+    ("H", "U"),
+    ("V", "I"),
+    ("A", "N"),
+    ("O", "B"),
+    ("E", "R"),
+    ("F", "S"),
+    ("L", "Y"),
+    ("P", "C"),
+    ("Z", "M"),
+];
+pub fn can_make_word(word: &str) -> bool {
+    let mut word = word.to_uppercase();
+    for (a, b) in LETTER_PAIRS.iter() {
+        println!("{} {}", a, b);
+        if word.contains(a) {
+            word = word.replacen(a, "", 1);
+            continue;
+            // We can only use one of the letters in the pair
+        }
+        if word.contains(b) {
+            word = word.replacen(b, "", 1);
+            continue;
+            // Just to make it match the other case
+        }
+    }
+    word.is_empty()
 }
--- abcproblem/rust/tests/lib_tests.rs
+++ abcproblem/rust/tests/lib_tests.rs
@@ -10,7 +10,23 @@
 //
 // ---- End of Test Outline ----
 
+// Task
+// Write a function that takes a string (word) and determines whether the word can be spelled with the given collection of blocks.
+// The rules are simple:
+// 1.Once a letter on a block is used that block cannot be used again
+// 2.The function should be case-insensitive
+// 3.Show the output on this page for the following 7 words in the following example
+
+use abc::*;
+
 #[test]
-fn test_lib_test() {
-    assert_eq!(1, 1);
+fn test_can_make_word() {
+    // Test that the library is able to actually solve the abc problem
+    assert_eq!(can_make_word("A"), true);
+    assert_eq!(can_make_word("BARK"), true);
+    assert_eq!(can_make_word("BOOK"), false);
+    assert_eq!(can_make_word("TREAT"), true);
+    assert_eq!(can_make_word("COMMON"), false);
+    assert_eq!(can_make_word("SQUAD"), true);
+    assert_eq!(can_make_word("CONFUSE"), true);
 }
